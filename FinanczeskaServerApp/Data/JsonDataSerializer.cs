a477092dfb3a9c35fd5c0f6f7f9158ed23f869dd
wikinto <41481527+wikinto@users.noreply.github.com>
2023-09-30 15:14:09 +0200

    json data serializer

---
+++ FinanczeskaServerApp/Data/JsonDataSerializer.cs
@@ -0,0 +1,37 @@
+﻿namespace FinanczeskaServerApp.Data
+{
+    public class JsonDataSerializer
+    {
+        //class for serializing data to json
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        //method for serializing data to json
+        public static void SerializeToJson(int id, string question, string answer)
+        {
+            //create a json string from the object
+            var jsonDataSerializer = new JsonDataSerializer
+            {
+                Id = id,
+                Question = question,
+                Answer = answer
+            };
+
+
+            var json = System.Text.Json.JsonSerializer.Serialize(jsonDataSerializer);
+
+            //if file exists, append to it
+            if (System.IO.File.Exists($"Data/{id}-{DateTime.Now:yyyy-MM-dd}.json"))
+            {
+                System.IO.File.AppendAllText($"Data/{id}-{DateTime.Now:yyyy-MM-dd}.json", json);
+                return;
+            }
+            else
+            {
+                System.IO.File.WriteAllText($"Data/{id}-{DateTime.Now:yyyy-MM-dd}.json", json);
+                return;
+            }
+        }
+    }
+}
