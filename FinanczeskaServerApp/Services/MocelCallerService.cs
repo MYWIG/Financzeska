197b9a7572fe0f03634d65946afaf22d271a85b0
Igor Markiv <imarkiv@biurohelixsolutions.onmicrosoft.com>
2023-09-30 17:47:24 +0200

    Sognal R + init ModelCaller

---
+++ FinanczeskaServerApp/Services/MocelCallerService.cs
@@ -0,0 +1,18 @@
+﻿namespace FinanczeskaServerApp.Services
+{
+    // This Sevice is call model
+    public class MocelCallerService
+    {
+        private readonly IHttpClient _httpClient;
+
+        public MocelCallerService()
+        {
+        }
+
+        public async Task AskModel(string userId, string UserInputData)
+        {
+
+        }
+
+    }
+}
