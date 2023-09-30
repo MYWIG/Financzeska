f9614ff7f7cd6aef243aae5c85bf935e4c8a5748
3DDrow <oleshkevychy@gmail.com>
2023-09-30 22:04:06 +0200

    Worked api for LLM. Without ability to emulate memory

--- csvParser/Program.cs
+++ csvParser/Program.cs
@@ -1,49 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace csvParser
 {
     internal class Program
     {
-        static void Main()
+        private static async Task Main()
         {
-            // Read the CSV file
-            string[] lines = File.ReadAllLines("E:\\Downloads\\1.csv");
+            // Define the API endpoint URL
+            string apiUrl = "http://46.170.221.65:5001/api/v1/generate/";
+            string parsedString = "Imie mojego kolegi : Jaroslav";
+            string clientMessege = "Jak ma na imie mój kolega?";
+            // Define the JSON data to send to the server
+            string jsonData = $@"{{
+                ""n"": 1,
+                ""max_context_length"": 4096,
+                ""max_length"": 100,
+                ""rep_pen"": 1.1,
+                ""temperature"": 0.7,
+                ""top_p"": 0.92,
+                ""top_k"": 0,
+                ""top_a"": 0,
+                ""typical"": 1,
+                ""tfs"": 1,
+                ""rep_pen_range"": 320,
+                ""rep_pen_slope"": 0.7,
+                ""sampler_order"": [6, 0, 1, 3, 4, 2, 5],
+                ""prompt"": ""[{parsedString}]\n\n{clientMessege}"",
+                ""quiet"": true,
+                ""stop_sequence"": [""You: "", ""\nYou "", ""\nKoboldAI: ""]
+            }}";
 
-            List<string> parsedData = new List<string>();
+            // Make the POST request
+            string apiResponse = await PostDataToServerAsync(apiUrl, jsonData);
 
-            foreach (string line in lines)
+            if (apiResponse != null)
             {
-                string[] parts = line.Split(';');
-
-                if (parts.Length < 4)
-                    continue; // Skip lines with insufficient data
-
-                string indicator = parts[0].Trim();
-                string unit = parts[2].Trim();
-
-                List<string> dataValues = new List<string>();
-
-                for (int i = 3; i < parts.Length; i++)
-                {
-                    // Replace spaces with dots and remove non-digit characters
-                    string cleanedValue = Regex.Replace(parts[i], @"[^\d.]", "").Replace(".", "").Insert(1, ".");
-                    dataValues.Add(cleanedValue);
-                }
-
-                string formattedData = string.Join("; ", dataValues.Zip(Enumerable.Range(1995, dataValues.Count), (v, y) => $"{v} {unit}, {y}"));
-                string result = $"{indicator} w {unit} wynosiło; {formattedData}";
-
-                parsedData.Add(result);
+                Console.WriteLine(jsonData);
+                Console.WriteLine("API Response:");
+                Console.WriteLine(apiResponse);
             }
+        }
 
-            string parsedDataStr = string.Join("\n", parsedData);
+        private static async Task<string> PostDataToServerAsync(string apiUrl, string jsonContent)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    // Define the content type as application/json
+                    httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+                    httpClient.DefaultRequestHeaders.Add("ContentType", "application/json");
 
-            // Print or save the parsed data as needed
-            Console.WriteLine(parsedDataStr);
+                    // Create a StringContent object with the JSON data
+                    var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                    // Send a POST request with the JSON data
+                    HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        return responseContent;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: {response.StatusCode}");
+                        return null;
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    Console.WriteLine($"HttpRequestException: {ex.Message}");
+                    return null;
+                }
+            }
         }
     }
 }
