9e5851914367301ee01d33fc20a164aab4b93ee3
Igor Markiv <imarkiv@biurohelixsolutions.onmicrosoft.com>
2023-10-01 06:31:18 +0200

    fix scrol

--- FinanczeskaServerApp/Program.cs
+++ FinanczeskaServerApp/Program.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Hosting;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -15,15 +18,22 @@ builder.Services.AddTransient<ModelCallerService>();
 builder.Services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
 
 
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders(); // Remove other logging providers
+    loggingBuilder.SetMinimumLevel(LogLevel.Trace); // Adjust log level as needed
+
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
 
