5b33739b54ab7706430b6f5b1b58bbf28c20525a
3DDrow <oleshkevychy@gmail.com>
2023-10-01 02:47:09 +0200

    parsing of file and return from api

--- csvParser/Program.cs
+++ csvParser/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace csvParser
 {
@@ -13,38 +12,68 @@ namespace csvParser
         private static async Task Main()
         {
             // Define the API endpoint URL
-            string apiUrl = "http://46.170.221.65:5001/api/v1/generate/";
-            string parsedString = "Imie mojego kolegi : Jaroslav";
-            string clientMessege = "Jak ma na imie mój kolega?";
-            // Define the JSON data to send to the server
-            string jsonData = $@"{{
-                ""n"": 1,
-                ""max_context_length"": 4096,
-                ""max_length"": 100,
-                ""rep_pen"": 1.1,
-                ""temperature"": 0.7,
-                ""top_p"": 0.92,
-                ""top_k"": 0,
-                ""top_a"": 0,
-                ""typical"": 1,
-                ""tfs"": 1,
-                ""rep_pen_range"": 320,
-                ""rep_pen_slope"": 0.7,
-                ""sampler_order"": [6, 0, 1, 3, 4, 2, 5],
-                ""prompt"": ""[{parsedString}]\n\n{clientMessege}"",
-                ""quiet"": true,
-                ""stop_sequence"": [""You: "", ""\nYou "", ""\nKoboldAI: ""]
-            }}";
+            string apiUrl = "http://46.170.221.65:5001/api/v1/generate";
+            string filePath = "E:\\Downloads\\Pomockryzysowa2021ŻródłaPomocy.csv";
 
-            // Make the POST request
-            string apiResponse = await PostDataToServerAsync(apiUrl, jsonData);
-
-            if (apiResponse != null)
+            try
             {
+                string formattedCsv = ParseCsvAndFormat(filePath);
+
+                string clientMessege = "Ile pieniedzy w srodkach zagranicznych?";
+
+                // Define the JSON data to send to the server
+                string jsonData = $@"{{
+                    ""n"": 1,
+                    ""max_context_length"": 4096,
+                    ""max_length"": 100,
+                    ""rep_pen"": 1.1,
+                    ""temperature"": 0.7,
+                    ""top_p"": 0.92,
+                    ""top_k"": 0,
+                    ""top_a"": 0,
+                    ""typical"": 1,
+                    ""tfs"": 1,
+                    ""rep_pen_range"": 320,
+                    ""rep_pen_slope"": 0.7,
+                    ""sampler_order"": [6, 0, 1, 3, 4, 2, 5],
+                    ""prompt"": ""[Jesteś AI. Odpowiadasz nie więcej, niż 15 słów. {formattedCsv}]\n\n{clientMessege}"",
+                    ""quiet"": true,
+                    ""stop_sequence"": [""You: "", ""\nYou "", ""\nKoboldAI: ""]
+                }}";
+
+                // Make the POST request
                 Console.WriteLine(jsonData);
-                Console.WriteLine("API Response:");
-                Console.WriteLine(apiResponse);
+                string apiResponse = await PostDataToServerAsync(apiUrl, jsonData);
+
+                if (apiResponse != null)
+                {
+                    Console.WriteLine("API Response:");
+                    Console.WriteLine(apiResponse);
+                }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            // Define the text pattern to parse
+
+            // Use regular expressions to extract the data
+               
+
+        }
+
+        static string ParseCsvAndFormat(string filePath)
+        {
+            // Read all text from the CSV file
+            string csvText = File.ReadAllText(filePath);
+
+            // Remove tabulations (tab characters) using regex
+            string cleanedCsv = Regex.Replace(csvText, @"\t", "");
+
+            // Replace '\r\n' (carriage return and newline) with a dot
+            cleanedCsv = cleanedCsv.Replace("\r\n", ". ");
+
+            return cleanedCsv;
         }
 
         private static async Task<string> PostDataToServerAsync(string apiUrl, string jsonContent)
